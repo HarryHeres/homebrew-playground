73344c082379a2b3aaef3c661fae55cbe6a9a0e2
HarryHeres <personal@janheres.eu>
2024-09-01 18:58:30 +0200

    Updating zen-browser cask

--- Casks/zen-browser.rb
+++ Casks/zen-browser.rb
@@ -1,14 +1,11 @@
 cask("zen-browser") do
   arch(arm: "aarch64", intel: "x64")
   version("1.0.0-a.34")
-  sha256(
-    arm: "6cc6e72c4a6a11d0139489d438ccf811715e170eebc01da739da32ace2580747",
-    intel: "b9cb48f21a4ee39f3f3ae2361b9b0a55ae7219638cf798e1483c0eb8cf4660a6"
-  )
+  sha256(arm: "6cc6e72c4a6a11d0139489d438ccf811715e170eebc01da739da32ace2580747", intel: "b9cb48f21a4ee39f3f3ae2361b9b0a55ae7219638cf798e1483c0eb8cf4660a6")
 
   url("https://github.com/zen-browser/desktop/releases/download/#{version}/zen.macos-#{arch}.dmg")
   name("Zen Browser")
-  desc("Beautifully designed, privacy-focused browser packed with awesome jfeatures")
+  desc("Beautifully designed, privacy-focused browser packed with awesome features")
   homepage("https://zen-browser.app/")
 
   livecheck do
@@ -16,12 +13,10 @@ cask("zen-browser") do
     regex(/[0-9].*-a.[0-9]*/i)
   end
 
-  zap = [
+  zap trash: [
     "$HOME/Library/Preferences/org.mozilla.com.zen.browser.plist",
     "$HOME/Library/Saved Application State/org.mozilla.com.zen.browser.savedState"
   ]
-
-  depends_on = []
 
   app("Zen Browser.app")
 
@@ -29,3 +24,4 @@ cask("zen-browser") do
     system("xattr -c '/Applications/Zen Browser.app/'")
   end
 end
+
